b8b503ba3e4ac5161fa42955584254b18ee48e7a
Thomas Paillet <75587285+ThomasPaillet@users.noreply.github.com>
2020-12-08 13:45:28 +0100

    Update cameras_set.c

--- cameras_set.c
+++ cameras_set.c
@@ -768,7 +768,7 @@ void show_cameras_set_configuration_window (void)
 				for (l = 1; rcp->ip_adresse[l] != '.'; l++) {}
 				cameras_configuration_widgets[i].ip_entry_buffer[0] = gtk_entry_buffer_new (rcp->ip_adresse, l);
 				k = l + 1;
-			} else cameras_configuration_widgets[i].ip_entry_buffer[0] = gtk_entry_buffer_new ("10", 2);
+			} else cameras_configuration_widgets[i].ip_entry_buffer[0] = gtk_entry_buffer_new (NULL, -1);
 
 			cameras_configuration_widgets[i].ip_entry[0] = gtk_entry_new_with_buffer (cameras_configuration_widgets[i].ip_entry_buffer[0]);
 			g_signal_connect (G_OBJECT (cameras_configuration_widgets[i].ip_entry[0]), "key-press-event", G_CALLBACK (digit_key_press), NULL);
@@ -875,7 +875,7 @@ void show_cameras_set_configuration_window (void)
 		gtk_widget_set_margin_bottom (cameras_configuration_widgets[i].name_entry, WINDOW_MARGIN_VALUE);
 		gtk_grid_attach (GTK_GRID (cameras_set_configuration_window_grid), cameras_configuration_widgets[i].name_entry, 1, j, 1, 1);
 
-		cameras_configuration_widgets[i].ip_entry_buffer[0] = gtk_entry_buffer_new ("10", 2);
+		cameras_configuration_widgets[i].ip_entry_buffer[0] = gtk_entry_buffer_new (NULL, -1);
 		cameras_configuration_widgets[i].ip_entry[0] = gtk_entry_new_with_buffer (cameras_configuration_widgets[i].ip_entry_buffer[0]);
 		gtk_entry_set_max_length (GTK_ENTRY (cameras_configuration_widgets[i].ip_entry[0]), 3);
 		gtk_entry_set_width_chars (GTK_ENTRY (cameras_configuration_widgets[i].ip_entry[0]), 3);
